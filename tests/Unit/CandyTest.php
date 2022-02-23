df9efe6ee890b770a7cffd515f9cd2af21a09587
Erdodif <erdodif@gmail.com>
2022-02-23 14:09:06 +0100

    Tesztadatok segédfüggvénybe kiemelése

--- tests/Unit/CandyTest.php
+++ tests/Unit/CandyTest.php
@@ -11,6 +11,51 @@ class CandyTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function fill_data_only_chocho(){
+        Candy::factory()->createMany([
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 5.5,
+                'sugar_content' => 2,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 5.1,
+                'sugar_content' => 3,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 15.2,
+                'sugar_content' => 10,
+            ],
+        ]);
+    }
+
+    public function fill_data_only_non_chocho(){
+        Candy::factory()->createMany([
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 0,
+                'sugar_content' => 13,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 0,
+                'sugar_content' => 0,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 0,
+                'sugar_content' => 2.3,
+            ],
+        ]);
+    }
+
+    public function fill_data_multipart(){
+        $this->fill_data_only_chocho();
+        $this->fill_data_only_non_chocho();
+    }
+
     public function test_one_chocho_candy(){
         $candy = new Candy([
             'name' => 'Chocholate',
@@ -23,58 +68,32 @@ class CandyTest extends TestCase
 
     public function test_multi_chocho_candy(){
 
-        Candy::factory()->createMany([
-            [  
-                'name' => 'Chocholate',
-                'cocoa_content' => 5.5,
-                'sugar_content' => 2,
-            ],
-            [  
-                'name' => 'Chocholate',
-                'cocoa_content' => 5.1,
-                'sugar_content' => 3,
-            ],
-            [  
-                'name' => 'Chocholate',
-                'cocoa_content' => 15.2,
-                'sugar_content' => 10,
-            ],
-        ]);
-        $this->assertEquals(5,Candy::averageSugarWithCocoa());
+        $this->fill_data_only_non_chocho();
+        $this->assertEquals(15,Candy::averageSugarWithCocoa());
     }
     
     public function test_multi_chocho_candy_with_free(){
-        Candy::factory()->createMany([
-            [  
-                'name' => 'Chocholate',
-                'cocoa_content' => 5.5,
-                'sugar_content' => 2,
-            ],
-            [  
-                'name' => 'Chocholate',
-                'cocoa_content' => 5.1,
-                'sugar_content' => 3,
-            ],
-            [  
-                'name' => 'Chocholate',
-                'cocoa_content' => 0,
-                'sugar_content' => 13,
-            ],
-            [  
-                'name' => 'Chocholate',
-                'cocoa_content' => 15.2,
-                'sugar_content' => 10,
-            ],
-            [  
-                'name' => 'Chocholate',
-                'cocoa_content' => 0,
-                'sugar_content' => 0,
-            ],
-        ]);
-        $this->assertEquals(5,Candy::averageSugarWithCocoa());
+        $this->fill_data_multipart();
+        $this->assertEquals(15,Candy::averageSugarWithCocoa());
     }
 
     public function test_no_chocholate(){
         $this->assertNan(Candy::averageSugarWithCocoa());
+    }
+
+    public function test_chocho_count_only_chocho(){
+
+    }
+
+    public function test_chocho_count_multipart(){
+        
+    }
+
+    public function test_chocho_count_only_non_chocho(){
+        
+    }
+
+    public function test_chocho_count_only_empty(){
+        
     }
 }
