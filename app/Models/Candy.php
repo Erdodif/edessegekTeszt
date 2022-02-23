e2f872354ff415ddc60506331fdb3c53713cf66c
Erdodif <erdodif@gmail.com>
2022-02-23 13:07:25 +0100

    Metódus és teszt elkészítése

--- app/Models/Candy.php
+++ app/Models/Candy.php
@@ -9,8 +9,22 @@ class Candy extends Model
 {
     use HasFactory;
 
-    static public function averageSugarWithCocoa()
+    protected $fillable = ["name", "cocoa_content", "sugar_content"];
+
+    static public function averageSugarWithCocoa(): float
     {
-        
+        $candies = Candy::all();
+        $sum = 0;
+        $count = 0;
+        foreach ($candies as $candy ){
+            if($candy->cocoa_content > 0){
+                $sum += $candy->sugar_content;
+                $count++;
+            }
+        }
+        if ($count === 0){
+            return NAN;
+        }
+        return floatval($sum) / $count;
     }
 }
---
+++ tests/Unit/CandyTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Candy;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class CandyTest extends TestCase
+{
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
+    public function test_one_chocho_candy(){
+        $candy = new Candy([
+            'name' => 'Chocholate',
+            'cocoa_content' => 5.5,
+            'sugar_content' => 2,
+        ]);
+        $this->assertTrue($candy->save());
+        $this->assertEquals(2,Candy::averageSugarWithCocoa());
+    }
+
+    public function test_multi_chocho_candy(){
+
+        Candy::factory()->createMany([
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 5.5,
+                'sugar_content' => 2,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 5.1,
+                'sugar_content' => 3,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 15.2,
+                'sugar_content' => 10,
+            ],
+        ]);
+        $this->assertEquals(5,Candy::averageSugarWithCocoa());
+    }
+    
+    public function test_multi_chocho_candy_with_free(){
+        Candy::factory()->createMany([
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 5.5,
+                'sugar_content' => 2,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 5.1,
+                'sugar_content' => 3,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 0,
+                'sugar_content' => 13,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 15.2,
+                'sugar_content' => 10,
+            ],
+            [  
+                'name' => 'Chocholate',
+                'cocoa_content' => 0,
+                'sugar_content' => 0,
+            ],
+        ]);
+        $this->assertEquals(5,Candy::averageSugarWithCocoa());
+    }
+
+    public function test_no_chocholate(){
+        $this->assertNan(Candy::averageSugarWithCocoa());
+    }
+}
